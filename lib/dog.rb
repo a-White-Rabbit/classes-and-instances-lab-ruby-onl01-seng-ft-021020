3d47547827ddbfa6465384d1b3aceead091693a6
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-17 21:28:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,7 @@
 class Dog 
-  # fido = Dog.new
-  # lassie = Dog.new 
-  # snoopy = Dog.new 
+  
 end
+
 fido = Dog.new
-  lassie = Dog.new 
-  snoopy = Dog.new 
+lassie = Dog.new 
+snoopy = Dog.new 
